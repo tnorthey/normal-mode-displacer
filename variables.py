7997c3d804708a32d915256b7e381d803394a393
thomas <northeyt@live.ie>
2018-10-17 15:23:37 +0100

    .

--- variables.py
+++ variables.py
@@ -1,18 +1,18 @@
 
 # Number of random geometries to generate
-N = 1000
+N = 1
 
 # switch on (1) or off (0) random displacements
-randm = 1
+randm = 0
 
 # if randm is on the random displacement will be in range [-a,a], if randm is off it will displace by exactly a
-a = 3
+a = 1
 
 # frequencies (cm-1) of selected modes
 freqcm1 = [1722.4496, 3799.4476, 3925.0128] 
 
 # selected modes
-#Modes = [1,2,3]
-nmodes = len(freqcm1)
-Modes = list(range(1, nmodes + 1 ))  # all modes
+Modes = 1
+#nmodes = len(freqcm1)
+#Modes = list(range(1, nmodes + 1 ))  # all modes
 
